216b4c794272d0d55a885302482cff08d7bf013b
gfrancesco <gfrancesco@gmail.com>
2018-11-02 11:27:23 +0100

    ADD: activity 5 code

---
+++ activity_5/act_5.ino
@@ -0,0 +1,19 @@
+const int ledPin = 9;
+
+
+void setup() {
+  // Init serial communication
+
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  float duration;
+
+  digitalWrite(ledPin, HIGH);
+  delay(3000);
+
+  digitalWrite(ledPin, LOW);
+  delay(3000);
+}
