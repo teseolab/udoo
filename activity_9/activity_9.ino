c3e22946a4223cf1838d2e6e1b07250a976c5945
gfrancesco <gfrancesco@gmail.com>
2018-11-05 12:27:43 +0100

    ADD: activity 9 code

---
+++ activity_9/activity_9.ino
@@ -0,0 +1,54 @@
+int SensorReadingcm;
+#define UltrasonicTriggerPin 5
+#define UltrasonicEchoPin 2
+#define ledGreenPin 9
+#define ledRedPin 13
+#define limit 20
+
+void setup() {
+  // Init serial communication
+
+  Serial.begin(115200);
+
+  pinMode(UltrasonicTriggerPin, OUTPUT);
+  pinMode(UltrasonicEchoPin, INPUT);
+
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(ledRedPin, OUTPUT);
+  
+}
+
+void loop() {
+  float duration;
+
+
+
+  digitalWrite(UltrasonicTriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(UltrasonicTriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(UltrasonicTriggerPin, LOW);
+
+  duration = pulseIn(UltrasonicEchoPin, HIGH);
+
+  SensorReadingcm = microsecondsToCentimeters(duration);
+
+  if (SensorReadingcm < limit ) {
+    digitalWrite(ledRedPin, HIGH);
+    digitalWrite(ledGreenPin, LOW);
+  } else {
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);
+  }
+  Serial.println(SensorReadingcm);
+  delay(19000);
+
+}
+
+
+long microsecondsToCentimeters(long ms){
+  return ms / 29.154 / 2;
+  
+}
+
+
---
+++ activity_9/cloud.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# This program logs a Arduino Sensor on an Udoo Neo to a #Thingspeak Channel
+# To use, get a Thingspeak.com account, set up a channel,
+# and capture the Channel Key at #https://thingspeak.com/docs/tutorials/
+# Then paste channel ID in the code for the value of "key" below.
+# Then run as sudo python   FileName1.py
+import httplib, urllib
+import time
+import serial
+# seconds to sleep between posts to the channel, API #limit min 15 sec
+sleep = 20
+#'Put your Thingspeak Channel Key here'
+# Thingspeak channel to update
+key = '7D30EE5AN79TNLK9'
+DEBUG = False
+# set to True to get more debug prints in the Python console
+DEBUG = False # set to True to get more debug prints in the Python console
+#Create serial device for reading serial
+arduino = serial.Serial('/dev/ttyMCC',115200,timeout=0)
+arduino.flushOutput()
+arduino.flushInput()
+serial_buffer = ""
+line = ""
+# sensor coefficient name
+sensorValue1 = '0'
+temp1 = 0
+#init for arduino so Arduino knows it can send data.
+arduino.write("I\n")
+def ReadArduino(arduino_out):
+    global serial_buffer, line
+    arduino_out = ""
+serial_data = arduino.read()
+while serial_data:
+# Read 1 character
+# If there is a character proceed
+# Add character to buffer
+serial_buffer += serial_data
+if DEBUG: print "ReadArduino:Serial buffer:",(repr(serial_buffer))
+# if there is a new line in it, then create Line string
+if "\r\n" in serial_buffer:
+    if DEBUG: print "ReadArduino:New Line:",(repr(serial_buffer))
+    line = serial_buffer
+    serial_buffer = "" # empty Serial buffer
+serial_data = arduino.read()
+# if there is a Line string then finish reading the Line
+if line:
+# strip string for unwanted characters
+line = line.replace("\r\n", "")
+                #line = line.strip(",")
+                arduino_out = line
+                if DEBUG: print "ReadArduino:arduino_out:", arduino_out
+                line = ""
+                if arduino_out == "": arduino_out = None
+                # add the last character to the new buffer
+                serial_buffer += serial_data
+                return arduino_out
+#Report Udoo Neo Arduino Sensor data to Thingspeak Channel
+def writesensordata():
+    global temp1, sensorValue1
+    while True:
+        #read arduino data and make it a integer value
+        sensorValue1 = ReadArduino(sensorValue1)
+        if sensorValue1 is None:
+            sensorValue1 = '0'
+        temp1 = (sensorValue1)
+		params = urllib.urlencode({'team_1': temp1,'key':key })
+		headers = {"Content-type": "application/x-www-form- urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print temp1
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+        except:
+            print "connection failed"
+        break
+#sleep for desired amount of time
+if __name__ == "__main__":
+        while True:
+                writesensordata()
+                time.sleep(sleep)
+        sleep(1800)
